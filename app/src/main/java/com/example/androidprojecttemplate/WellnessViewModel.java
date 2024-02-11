78b40ffb80b45d6ecdb2b5ab1ce0cd8834712877
aujjina3 <114704267+aujjina3@users.noreply.github.com>
2024-02-11 14:32:27 +0100

    Added my work to WellnessViewModel.java

--- app/src/main/java/com/example/androidprojecttemplate/WellnessViewModel.java
+++ app/src/main/java/com/example/androidprojecttemplate/WellnessViewModel.java
@@ -28,8 +28,8 @@ public class WellnessViewModel {
 
     // TODO 2 : Implement updateData function using helpers in wellnessData to update model
     public void updateData(int sleep, int fitness) {
-        wellnessData.setFitnessMinutes(fitness);
         wellnessData.setSleepHours(sleep);
+        wellnessData.setFitnessMinutes(fitness);
     }
 }
 
